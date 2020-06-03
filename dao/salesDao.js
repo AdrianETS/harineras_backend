f0b306cfa9b1263dbbbf6697124b2d79b2ce8fc5
Adrian <adrian.belver@etservices.eu>
2020-06-03 13:07:03 +0200

    Feature/16.-Acceso a lectura de ventas
    Backend devuelve precio total por venta

--- dao/salesDao.js
+++ dao/salesDao.js
@@ -6,15 +6,34 @@ const con = mysql.createConnection(constants.connection);
 
 function getSaleByClientId(id) {
     return new Promise((resolve, reject) => {
-        con.query("SELECT venta.id AS venta, venta.fecha , productos.nombre_comercial AS producto, detalle_venta.cantidad_pedida AS cantidad, detalle_venta.precio_unitario " + 
+        con.query("SELECT venta.id AS venta, venta.fecha , productos.nombre_comercial AS producto, detalle_venta.cantidad_pedida AS cantidad, detalle_venta.precio_unitario, cantidad_pedida*precio_unitario AS precio_total " + 
         "FROM detalle_venta INNER JOIN venta ON venta.id=detalle_venta.id_venta " +
         "INNER JOIN productos ON  detalle_venta.id_producto = productos.id " + 
         "WHERE venta.cliente = " +  mysql.escape(id) +
         " ORDER BY venta.fecha" , function (err, result, fields) {
             if (err) throw err;
-            resolve(result);
+            let convertedResult = calculateTotalPrice(result);
+            resolve(convertedResult);
           });
     })
 }
+
+function calculateTotalPrice(array){
+    let totalSumPerSale = 0
+    for(let i = 0; i < array.length; i++ ){
+        if(i != array.length - 1 && array[i].venta == array[i+1].venta){
+            totalSumPerSale = totalSumPerSale + array[i].precio_total;
+            array[i].precio_total_venta ="";
+        }
+        else {
+            totalSumPerSale = totalSumPerSale + array[i].precio_total
+            array[i].precio_total_venta = totalSumPerSale;
+            totalSumPerSale = 0;
+        }
+    }
+    return array;
+}
+
+
 
 module.exports = {getSaleByClientId}
