5017f86e6e6c57621acc9fd1c45beab20dda9265
Merche <mercedes.rodriguez@movicoders.com>
2020-07-02 17:19:11 +0200

    Minor fix
    modified query getAllSales

--- dao/salesDao.js
+++ dao/salesDao.js
@@ -21,7 +21,7 @@ function getSaleByClientId(id) {
 
 function getAllSales() {
     return new Promise((resolve, reject) => {
-        con.query("SELECT venta.id AS venta, venta.fecha , productos.nombre_comercial AS producto, detalle_venta.cantidad_pedida AS cantidad, detalle_venta.precio_unitario, cantidad_pedida*precio_unitario AS precio_total " +
+        con.query("SELECT venta.id AS venta, venta.fecha , productos.nombre_comercial AS producto, detalle_venta.cantidad_pedida AS cantidad, detalle_venta.precio_unitario, cantidad_pedida*precio_unitario AS precio_total, venta.cliente as client " +
             "FROM detalle_venta INNER JOIN venta ON venta.id=detalle_venta.id_venta " +
             "INNER JOIN productos ON  detalle_venta.id_producto = productos.id " +
             " ORDER BY venta.fecha", function (err, result, fields) {
